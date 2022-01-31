92943fac596a0961ea5ce282e7199f4d31db4bf5
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-01-31 18:24:55 +0100

    Añadida transicion de incio y pulsa Start modificada añadiendo otro .gif mas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const screenTitle = document.getElementById('inicio');
 const transicionInicio = document.getElementById('transicionInicio')
+const pulsaStart = document.getElementById('pulsaStartInicio')
 
 // Funciones 
 
@@ -17,6 +18,7 @@ const cambiaPantalla = (cambio) => {
         document.getElementById(pantalla).style.display = "none";
     }
 
+    //Animacion para transicion entre inicio y pulsaStart
     if (pantallaDeseada == "screen2") {
         transicionInicio.style.backgroundImage = "url('assets/img/intro-1.gif')";
         transicionInicio.style.backgroundSize = "cover"
@@ -29,15 +31,20 @@ const cambiaPantalla = (cambio) => {
                 setTimeout(function() {
                     transicionInicio.style.backgroundSize = "cover"
                     transicionInicio.style.backgroundImage = "url('assets/img/intro-3.gif')";
-                    cambiaPantalla(3)
+                    setTimeout(function() {
+                        cambiaPantalla(3)
+                    },2500)
                 },2500)
             }, 2500)
         }, 2500);
     }
 }
 
-//TRansicion pantallas
+//TRansicion pantalla
 
+//Imagen para Pantalla inicio
 screenTitle.style.backgroundImage = "url('assets/img/titulo.png')"
 screenTitle.style.backgroundSize = "cover"
 
+pulsaStart.style.backgroundImage = "url('assets/img/press_start.png')"
+pulsaStart.style.backgroundImage = "cover"
